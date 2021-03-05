03102f60c84424c05012128bf79a538ff4ca3134
Neha Bhalekar <nehabhalekar18@gmail.com>
2021-03-05 01:27:37 +0100

    dubai locations, routes

---
+++ src/constants/dubaiLocationsData.js
@@ -0,0 +1,104 @@
+import SufouhBeach from "../assets/sufouh-beach-img.jpg";
+import BurjKhalifa from "../assets/burj-khalifa-img.jpg";
+import DubaiMall from "../assets/dubai-mall-img.jpg";
+import DubaiFountain from "../assets/dubai-fountain-img.jpg";
+import CoveBeachCasear from "../assets/cove-casear-beach-img.jpg";
+import LaMer from "../assets/la-mer-img.jpg";
+import DubaiImg from "../assets/dubai-img.jpg";
+import FranceImg from "../assets/france-img.jpg";
+import GermanyImg from "../assets/germany-img.jpg";
+import AustraliaImg from "../assets/australia-img.jpg";
+import NewZealandImg from "../assets/newZealand-img.jpg";
+const dubaiLocations = [
+  {
+    id: 1,
+    name: "Burj Khalifa",
+    subData: "10km from Dubai airport",
+    description:
+      "160-story skyscraper & observation deck,  skyscraper in Dubai. ",
+    img: BurjKhalifa,
+  },
+  {
+    id: 2,
+    name: "The Dubai Mall",
+    subData: "10km from Dubai airport",
+    description:
+      "26th-largest shopping mall in the world by gross leasable area, tying with West Edmonton Mall.",
+    img: DubaiMall,
+  },
+  {
+    id: 3,
+    name: "The Dubai Fountain",
+    subData: "10km from Dubai airport",
+    description:
+      "Huge, choreographed fountain complex.  Located on the 12 hectare manmade Burj Khalifa Lake",
+    img: DubaiFountain,
+  },
+
+  {
+    id: 4,
+    name: "Burj Khalifa",
+    subData: "10km from Dubai airport",
+    description:
+      "160-story skyscraper & observation deck,  skyscraper in Dubai. ",
+    img: BurjKhalifa,
+  },
+  {
+    id: 5,
+    name: "Sufouh Beach",
+    subData: "12km from Dubai airport",
+    description:
+      "Tranquil white sandy & palm lined beach, Soak up the scene at a trendy beach club",
+    img: SufouhBeach,
+  },
+  {
+    id: 6,
+    name: "Cove Beach Caesars",
+    subData: "12km from Dubai airport",
+    description:
+      "high-end dining and an energetic party scene. You can lounge by the pool and have lobster.",
+    img: CoveBeachCasear,
+  },
+  {
+    id: 8,
+    name: "La Mer",
+    subData: "12km from Dubai airport",
+    description:
+      "Perfect summer beach spot if u wanna avoid the hotel ± beach access concept!",
+    img: LaMer,
+  },
+  {
+    id: 9,
+    name: "Sufouh Beach",
+    subData: "12km from Dubai airport",
+    description:
+      "Tranquil white sandy & palm lined beach, Soak up the scene at a trendy beach club",
+    img: SufouhBeach,
+  },
+
+  {
+    id: 10,
+    name: "Germany",
+    subData: "Western European Country",
+    description:
+      "Royal Heritage, Literary London, Historic pubs, Markets Art Museum, Football.",
+    img: GermanyImg,
+  },
+  {
+    id: 11,
+    name: "New Zealand",
+    subData: "Island in Pacific Ocean",
+    description:
+      "Royal Heritage, Literary London, Historic pubs, Markets Art Museum, Football.",
+    img: NewZealandImg,
+  },
+  {
+    id: 12,
+    name: "Australia",
+    subData: "Country",
+    description:
+      "Royal Heritage, Literary London, Historic pubs, Markets Art Museum, Football.",
+    img: AustraliaImg,
+  },
+];
+export default dubaiLocations;
