37d72d040b3ac169691724c857dc1e76387b1be1
sayli19 <me.sayli19@gmail.com>
2021-03-06 02:22:10 +0100

    Login page - Sign up component

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,10 @@ import "./Login.css";
 import LoginBg from "../assets/login-img.jpg";
 import fb from "../assets/Facebook-Circled.png";
 import google from "../assets/Google.png";
-function Login() {
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+function Login({}) {
   return (
     <div className="login">
-      {/* <img src={LoginBg} alt="" className="login__img" /> */}
       <div class="row login__maincontainer">
         <div class="col-sm-6 card__content">
           <div className="login__content1"> EXPLORE NEARBY PLACES</div>
@@ -19,7 +19,9 @@ function Login() {
           </div>
         </div>
 
-        <div class="col-sm-6 card login__card ">
+        {/* LOGIN */}
+
+        {/* <div class="col-sm-6 card login__card ">
           <div className="loginCard__info">
             <div class="row login__cardtitle">
               <p>Login to continue exploring</p>
@@ -54,6 +56,7 @@ function Login() {
               <p className="card__password">Password</p>
               <div className="card__PasswordBox">
                 <input className="card__passBox_input" type="password" />
+                    <VisibilityOffIcon />
               </div>
             </div>
 
@@ -79,6 +82,59 @@ function Login() {
                 {" "}
                 <button type="button" class="btn btn-dark card__loginbtn">
                   Login
+                </button>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div> */}
+
+        {/* SIGN UP */}
+
+        <div class="col-sm-6 card login__card">
+          <div className="loginCard__info">
+            <div class="row login__cardtitle">
+              <p>Create account to continue exploring</p>
+            </div>
+
+            <div class="row card__inputs">
+              <p className="card__signUpemail">Email</p>
+              <div className="card__emailBox">
+                <input className="card__emailBox_input" type="text" />
+              </div>
+              <p className="card__signUpPwd">Password</p>
+              <div className="card__PasswordBox">
+                <input className="card__passBox_input" type="password" />
+                <VisibilityOffIcon />
+              </div>
+              <p className="card__signUpCpwd">Password</p>
+              <div className="card__PasswordBox">
+                <input className="card__passBox_input" type="password"></input>
+                <VisibilityOffIcon />
+              </div>
+            </div>
+
+            <div class="row card__inputDetails">
+              <div class="col-sm card__signIn">
+                {" "}
+                <input type="checkbox" /> By accepting I agree to terms and
+                privacy policies as mentioned.
+              </div>
+            </div>
+
+            <div class="row card__buttons">
+              <div class="col-sm-5 card__signUpCol">
+                <button
+                  type="button"
+                  class="btn btn-outline-primary card__signUpbtn">
+                  Log In
+                </button>
+              </div>
+              <div class="col-sm-2 card__btnSpace"></div>
+              <div class="col-sm-5 card__LoginCol">
+                {" "}
+                <button type="button" class="btn btn-dark card__loginbtn">
+                  Sign Up
                 </button>
               </div>
               <div></div>
