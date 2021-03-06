47ca2cf76ef5d4d5a2f456623da1b9512d1dfa82
sayli19 <me.sayli19@gmail.com>
2021-03-07 00:06:29 +0100

    completed trip, pop up

--- src/constants/tripCardListData.js
+++ src/constants/tripCardListData.js
@@ -8,84 +8,85 @@ const tripCardList = [
   {
     id: 1,
     tripName: "London Trip",
-    tripDate: "Mar 11- 19, 2020",
+    tripDate: "Mar 11 - 19, 2021",
     img: LondonImg,
   },
   {
     id: 2,
     tripName: "Dubai",
-    tripDate: "Feb 14- 20, 2020",
+    tripDate: "Mar 19 - 25, 2021",
     img: DubaiImg,
-  },
-  {
-    id: 3,
-    tripName: "France",
-    tripDate: "Jan 12 - 30, 2021",
-    img: FranceImg,
-  },
-
-  {
-    id: 4,
-    tripName: "Germany",
-    tripDate: "Feb 14- 20, 2020",
-    img: GermanyImg,
-  },
-  {
-    id: 5,
-    tripName: "New Zealand",
-    tripDate: "Jan 12 - 30, 2021",
-
-    img: NewZealandImg,
-  },
-  {
-    id: 6,
-    tripName: "Australia",
-    tripDate: "Nov 19 - 25, 2020",
-
-    img: AustraliaImg,
-  },
-  {
-    id: 7,
-    tripName: "London",
-    tripDate: "Jan 19 - 25, 2020",
-
-    img: LondonImg,
-  },
-  {
-    id: 8,
-    tripName: "Dubai",
-    tripDate: "Feb 14- 20, 2020",
-
-    img: DubaiImg,
-  },
-  {
-    id: 9,
-    tripName: "France",
-    tripDate: "Nov 19 - 25, 2020",
-
-    img: FranceImg,
-  },
-
-  {
-    id: 10,
-    tripName: "Germany",
-    tripDate: "Feb 14- 20, 2020",
-
-    img: GermanyImg,
-  },
-  {
-    id: 11,
-    tripName: "New Zealand",
-    tripDate: "Jan 12 - 30, 2021",
-
-    img: NewZealandImg,
-  },
-  {
-    id: 12,
-    tripName: "Australia",
-    tripDate: "Feb 14- 20, 2020",
-    img: AustraliaImg,
   },
 ];
 
 export default tripCardList;
+
+// {
+//   id: 3,
+//   tripName: "France",
+//   tripDate: "Jan 12 - 30, 2021",
+//   img: FranceImg,
+// },
+
+// {
+//   id: 4,
+//   tripName: "Germany",
+//   tripDate: "Feb 14- 20, 2020",
+//   img: GermanyImg,
+// },
+// {
+//   id: 5,
+//   tripName: "New Zealand",
+//   tripDate: "Jan 12 - 30, 2021",
+
+//   img: NewZealandImg,
+// },
+// {
+//   id: 6,
+//   tripName: "Australia",
+//   tripDate: "Nov 19 - 25, 2020",
+
+//   img: AustraliaImg,
+// },
+// {
+//   id: 7,
+//   tripName: "London",
+//   tripDate: "Jan 19 - 25, 2020",
+
+//   img: LondonImg,
+// },
+// {
+//   id: 8,
+//   tripName: "Dubai",
+//   tripDate: "Feb 14- 20, 2020",
+
+//   img: DubaiImg,
+// },
+// {
+//   id: 9,
+//   tripName: "France",
+//   tripDate: "Nov 19 - 25, 2020",
+
+//   img: FranceImg,
+// },
+
+// {
+//   id: 10,
+//   tripName: "Germany",
+//   tripDate: "Feb 14- 20, 2020",
+
+//   img: GermanyImg,
+// },
+// {
+//   id: 11,
+//   tripName: "New Zealand",
+//   tripDate: "Jan 12 - 30, 2021",
+
+//   img: NewZealandImg,
+// },
+// {
+//   id: 12,
+//   tripName: "Australia",
+//   tripDate: "Feb 14- 20, 2020",
+//   img: AustraliaImg,
+// },
