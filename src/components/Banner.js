3ae655eec7c6ed450ea092c5642ae1719d131559
sayli19 <me.sayli19@gmail.com>
2021-03-14 13:37:53 +0100

    Test cases for Banner and footer

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,19 +5,22 @@ import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 
 function Banner({ bannerImg, searchLocation }) {
   return (
-    <div className="banner">
-      <img src={bannerImg} alt="" className="banner__img" />
-      <div className="banner__search">
-        <LocationOnOutlinedIcon />
-        <input
-          type="text"
-          placeholder="Location"
-          defaultValue={searchLocation}
-        />
-        <SearchOutlinedIcon className="banner__search__icon" />
-      </div>
-      <div className="banner__content">
-        <h5>Never Stop Exploring</h5>
+    <div>
+      {" "}
+      <div className="banner">
+        <img src={bannerImg} alt="" className="banner__img" />
+        <div className="banner__search">
+          <LocationOnOutlinedIcon />
+          <input
+            type="text"
+            placeholder="Location"
+            defaultValue={searchLocation}
+          />
+          <SearchOutlinedIcon className="banner__search__icon" />
+        </div>
+        <div className="banner__content">
+          <h5>Never Stop Exploring</h5>
+        </div>
       </div>
     </div>
   );
